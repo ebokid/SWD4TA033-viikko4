b2e101f21b3eb240a2677e2230e9394d2b742d7a
Johanna <joq.paavola@gmail.com>
2021-04-25 16:43:45 +0300

    finished exercise

---
+++ Viikko4/src/control/Asiakkaat.java
@@ -0,0 +1,39 @@
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+import model.Asiakas;
+import model.dao.Dao;
+
+
+@WebServlet("/asiakkaat/*")
+public class Asiakkaat extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Asiakkaat() {
+        super();
+        System.out.println("Viiko4.Asiakkaat()");
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Asiakkaat.doGet()");
+		String pathInfo = request.getPathInfo();	//haetaan kutsun polkutiedot, esim. /audi			
+		String hakusana = pathInfo.replace("/", "");
+		Dao dao = new Dao();
+		ArrayList<Asiakas> asiakkaat = dao.listaaAsiakkaat(hakusana);
+		String strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.println(strJSON);		
+	}
+
+}
